66aa9d1b9d4d67d8c460a2a70f4b280f1678d0ac
Nataly Lucena <natalylucena.pb@gmail.com>
2022-09-18 21:45:23 -0300

    Implementando o Post Customer

--- src/main/kotlin/com/mercadolivro/controller/CustomerController.kt
+++ src/main/kotlin/com/mercadolivro/controller/CustomerController.kt
@@ -1,7 +1,10 @@
 package com.mercadolivro.controller
 
+import com.mercadolivro.controller.request.PostCustomerRequest
 import com.mercadolivro.model.CustomerModel
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
@@ -12,5 +15,10 @@ class CustomerController {
     @GetMapping
     fun getCustomer(): CustomerModel {
         return CustomerModel("1", "Nataly", "email@email.com")
+    }
+
+    @PostMapping
+    fun createCustomer(@RequestBody customer: PostCustomerRequest) {
+        println(customer)
     }
 }
--- src/main/kotlin/com/mercadolivro/model/CustomerModel.kt
+++ src/main/kotlin/com/mercadolivro/model/CustomerModel.kt
@@ -1,7 +1,6 @@
 package com.mercadolivro.model
 
 data class CustomerModel(
-
     var id: String,
     var name: String,
     var email: String
