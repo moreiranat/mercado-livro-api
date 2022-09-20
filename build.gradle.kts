31fd16939acc53259a5c660c196c8c2a65b30bff
Nataly Lucena <natalylucena.pb@gmail.com>
2022-09-20 16:03:05 -0300

    Atualização da dependência do Flyway

--- build.gradle.kts
+++ build.gradle.kts
@@ -23,7 +23,8 @@ dependencies {
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
 
-	implementation("org.flywaydb:flyway-core:7.7.0")
+	implementation("org.flywaydb:flyway-core:9.3.1")
+	implementation("org.flywaydb:flyway-mysql")
 
 	runtimeOnly("mysql:mysql-connector-java")
 
