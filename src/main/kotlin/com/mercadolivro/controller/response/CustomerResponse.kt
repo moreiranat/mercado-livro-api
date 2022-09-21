0f5cc42e3a9837351b4d62cd00e3d16191486d2a
Nataly Lucena <natalylucena.pb@gmail.com>
2022-09-21 19:39:02 -0300

    Alterando o retorno de BookModel e Customer Model

---
+++ src/main/kotlin/com/mercadolivro/controller/response/CustomerResponse.kt
@@ -0,0 +1,16 @@
+package com.mercadolivro.controller.response
+
+import com.mercadolivro.enums.CustomerStatus
+
+data class CustomerResponse(
+    var id: Int? = null,
+
+    var name: String,
+
+    var email: String,
+
+    var status: CustomerStatus
+)
+
+
+
