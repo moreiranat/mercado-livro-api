31ba033bec21bf1816582c5f7f0192a6a5cd8bda
Nataly Lucena <natalylucena.pb@gmail.com>
2022-09-21 18:50:34 -0300

    Bloqueando alteração de status de livros cancelados/deletados

--- src/main/kotlin/com/mercadolivro/model/BookModel.kt
+++ src/main/kotlin/com/mercadolivro/model/BookModel.kt
@@ -17,11 +17,25 @@ data class BookModel(
     @Column
     var price: BigDecimal,
 
-    @Column
-    @Enumerated(EnumType.STRING)
-    var status: BookStatus? = null, //status é nullable e o valor padrao dele esta como nulo
-
     @ManyToOne //muitos livros para um usuario
     @JoinColumn(name = "customer_id") //customer_id é referente ao identificador chave primaria que esta em CustomerModel
     var customer: CustomerModel? = null
-)
+) {
+    @Column
+    @Enumerated(EnumType.STRING)
+    var status: BookStatus? = null //status é nullable e o valor padrao dele esta como nulo
+        set(value) { //value = novo valor do atributo. Sobrescreve o metodo)
+            if(field == BookStatus.CANCELADO || field == BookStatus.DELETADO) {
+                throw Exception("Não é possível alterar um livro com status ${field}.name")
+            }
+            field = value //sobrescreve o novo valor
+        }
+
+    constructor(id: Int? = null,
+                name: String,
+                price: BigDecimal,
+                customer: CustomerModel? = null,
+                status: BookStatus?): this(id, name, price, customer) {
+        this.status = status
+    }
+}
