ba0c4d8dab5b7626ef4f7c7ae55eaf6fa538f158
Nataly Lucena <natalylucena.pb@gmail.com>
2022-09-22 21:15:02 -0300

    Adicionando validaçoes em outros endpoints

--- src/main/kotlin/com/mercadolivro/controller/BookController.kt
+++ src/main/kotlin/com/mercadolivro/controller/BookController.kt
@@ -12,6 +12,7 @@ import org.springframework.data.domain.Pageable
 import org.springframework.data.web.PageableDefault
 import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.*
+import javax.validation.Valid
 
 @RestController
 @RequestMapping("books")
@@ -22,7 +23,7 @@ class BookController (
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    fun create(@RequestBody request: PostBookRequest) {
+    fun create(@RequestBody @Valid request: PostBookRequest) {
         val customer = customerService.findById(request.customerId)
         bookService.createBook(request.toBookModel(customer))
     }
@@ -50,10 +51,9 @@ class BookController (
 
     @PutMapping("/{id}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
-    fun update(@PathVariable id: Int, @RequestBody book: PutBookRequest) {
+    fun update(@PathVariable id: Int, @RequestBody book: PutBookRequest) { //não precisa colocar @Valid os atributos de PutBookRequest são nullable, ou seja, podem não ser recebidos
         val bookSaved = bookService.findById(id)
         bookService.update(book.toBookModel(bookSaved))
-
     }
 
 
--- src/main/kotlin/com/mercadolivro/controller/request/PutBookRequest.kt
+++ src/main/kotlin/com/mercadolivro/controller/request/PutBookRequest.kt
@@ -4,6 +4,7 @@ import java.math.BigDecimal
 
 data class PutBookRequest(
     //pode ou não receber o name e o price; pode deixar o valor antigo ou o novo
+
     var name: String?,
     var price: BigDecimal?
 )
