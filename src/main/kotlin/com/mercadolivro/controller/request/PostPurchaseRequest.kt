f116c8630a173af6de2bf08598493a18725df420
Nataly Lucena <natalylucena.pb@gmail.com>
2022-09-23 11:45:12 -0300

    Disparando nosso primeiro Evento

--- src/main/kotlin/com/mercadolivro/controller/request/PostPurchaseRequest.kt
+++ src/main/kotlin/com/mercadolivro/controller/request/PostPurchaseRequest.kt
@@ -1,5 +1,6 @@
 package com.mercadolivro.controller.request
 
+import com.fasterxml.jackson.annotation.JsonAlias
 import javax.validation.constraints.NotNull
 import javax.validation.constraints.Positive
 
@@ -7,9 +8,11 @@ data class PostPurchaseRequest(
 
     @field: NotNull
     @field: Positive //numero maior que 0
+    @JsonAlias("customer_id") //usa essa notacao porque estamos enviando essas informacoes no snake case e estamos recebendo elas como em camel case
     val customerId: Int,
 
     @field: NotNull
+    @JsonAlias("book_ids")
     val bookIds: Set<Int>
 
 )
