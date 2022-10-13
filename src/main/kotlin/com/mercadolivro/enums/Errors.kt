a9a37935f1e872df42933342c173be9baee467e6
Nataly Lucena <natalylucena.pb@gmail.com>
2022-10-13 19:25:24 -0300

    Tratando Exception e Melhorando Retorno

--- src/main/kotlin/com/mercadolivro/enums/Errors.kt
+++ src/main/kotlin/com/mercadolivro/enums/Errors.kt
@@ -3,6 +3,7 @@ package com.mercadolivro.enums
 enum class Errors(val code: String, val message: String) {
 
     //range dos erros: de 100 em 100
+    ML000("ML-000", "Access Denied"),
     ML001("ML-001", "Invalid Request"),
     ML101("ML-101", "Book [%s] not exists"),
     ML102("ML-102", "Cannot update book with status [%s]"),
