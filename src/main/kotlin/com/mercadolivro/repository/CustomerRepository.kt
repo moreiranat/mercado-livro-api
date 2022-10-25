64a786c905803d27dad52413bbb22f0c34157616
Nataly Lucena <natalylucena.pb@gmail.com>
2022-10-25 20:45:04 -0300

    Testando um Repository

--- src/main/kotlin/com/mercadolivro/repository/CustomerRepository.kt
+++ src/main/kotlin/com/mercadolivro/repository/CustomerRepository.kt
@@ -1,8 +1,6 @@
 package com.mercadolivro.repository
 
 import com.mercadolivro.model.CustomerModel
-import org.springframework.data.domain.Page
-import org.springframework.data.domain.Pageable
 import org.springframework.data.jpa.repository.JpaRepository
 
 interface CustomerRepository : JpaRepository<CustomerModel, Int> {
